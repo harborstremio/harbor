43af8ac9f1a42bb887466a70f2aa237d79a1062d
theabdull5h <aboodbk10@gmail.com>
2026-08-03 04:22:20 +0300

    Remove TopFavoritesSection component
    s

--- src/components/profile/TopFavoritesSection.tsx
+++ src/components/profile/TopFavoritesSection.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
-
-export const TopFavoritesSection: React.FC = () => {
-        return React.createElement(
-                    'div',
-            { className: 'top-favorites' },
-                    React.createElement('h2', null, 'Top Favorites')
-                );
-};
-
-export default TopFavoritesSection;
+s
