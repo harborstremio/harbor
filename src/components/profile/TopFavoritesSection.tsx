231d837b50826f3d3dad547bb75fb46a0ac139f1
theabdull5h <aboodbk10@gmail.com>
2026-08-03 04:12:05 +0300

    Create TopFavoritesSection component

---
+++ src/components/profile/TopFavoritesSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const TopFavoritesSection: React.FC = () => {
+    return (
+          <div className="top-favorites">
+                <h2>Top Favorites</h2>h2>
+          </div>div>
+        );
+};
+
+export default TopFavoritesSection;</div>
